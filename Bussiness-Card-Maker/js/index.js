ca8d8cadd49007a0d5c8214d4b339fb7eb8cde8b
Chamod Shehanka <hcsperera@outlook.com>
2018-05-22 10:40:25 +0530

    JS Changed

--- Bussiness-Card-Maker/js/index.js
+++ Bussiness-Card-Maker/js/index.js
@@ -1,7 +1,8 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+/**
+ * Chamod Shehanka
+ *
+ * Business Card Maker
+ *
+ *
+ * **/
 
-
-console.log("Hello Java Script");
